248b36c1f033aeb09f7a9f52a2b7f1b13da46e61
KateScher <kat-m1@mail.ru>
2023-02-28 21:12:23 +0500

    Home Work task 1

---
+++ Lesson_1/HW/HW_1_1/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 2: Напишите программу, которая на вход принимает два числа 
+// и выдаёт, какое число большее, а какое меньшее.
+// a = 5; b = 7 -> max = 7
+// a = 2 b = 10 -> max = 10
+// a = -9 b = -3 -> max = -3
+
+string s_a = Console.ReadLine()!;
+string s_b = Console.ReadLine()!;
+int a = int.Parse(s_a);
+int b = int.Parse(s_b);
+
+if (a > b) Console.WriteLine("a = " + a + "," + " b = " + b + ", a > b");
+else Console.WriteLine("a = " + a + "," + " b = " + b + ", a < b");
+
+
---
+++ Lesson_1/HW/HW_1_2/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+// например:2, 3, 7 -> 7
+
+int a = int.Parse(Console.ReadLine()!);
+int b = int.Parse(Console.ReadLine()!);
+int c = int.Parse(Console.ReadLine()!);
+int more = 0;
+
+if (a > b) {
+    more = a;
+}
+else more = b;
+
+if (a > c) {
+    more = a;
+}
+else more = c;
+
+Console.WriteLine(more);
