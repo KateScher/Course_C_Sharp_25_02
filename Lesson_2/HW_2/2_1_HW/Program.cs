f2e61175e2d07bef9806142eccb8d26e635b39dc
KateScher <kat-m1@mail.ru>
2023-03-02 19:50:46 +0500

    Task 2_1_HW

---
+++ Lesson_2/HW_2/2_1_HW/Program.cs
@@ -0,0 +1,12 @@
+﻿// Задача 1: Напишите программу, которая принимает на вход трёхзначное число
+// и на выходе показывает вторую цифру этого числа. Например, 456 -> 5, 782 -> 8, 918 -> 1
+
+
+int TakeNum(int num)
+{
+    Console.WriteLine(num);
+    return ((num % 100) /10);
+}
+
+int result = TakeNum(new Random().Next(100, 1000));
+Console.WriteLine(result);
